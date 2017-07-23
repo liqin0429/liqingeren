19b49e5b9072909289d4244276396f6774a0e587
liqin0429 <1430961095@qq.com>
2017-07-23 18:46:29 +0800

    11

---
+++ html/juepinxm/js/gx-gouwuche.js
@@ -0,0 +1,9 @@
+$(function(){
+	let dian=$(".header .leimu2 .kuang .three");
+	let ping=$('.ping')
+//	dian.click(function(index){
+	dian.on("touchstart",function(index){
+		dian.removeClass('active').eq($(this).index()).addClass('active');
+		$(this).parents('.header').siblings('.ping').css('display','none').eq($(this).index()).css('display','block')
+	})
+})
