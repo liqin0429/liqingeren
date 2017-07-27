23a6997d5e10b36e498c2b5ebe26e069b9649202
liqin0429 <1430961095@qq.com>
2017-07-27 23:45:45 +0800

    11
    11

--- html/juepinxm/js/gx-search.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	let dian=$(".search-sousuo input")
-	dian.focus(function(){
-		$(this).parent().next(".xiala").eq($(this).index()).css("display","block");
-	})
-//	dian.blur(function(){
-//		$(this).parent().next(".xiala").eq($(this).index()).css("display","none");
-//	})
-})
